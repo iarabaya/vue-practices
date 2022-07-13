8d9ad3920a38dd0f917ea6828918338f9e23b887
iara baya <iarabayav@gmail.com>
2022-07-12 23:08:05 -0300

    first app vue

---
+++ vue-fherrera/03-fundamentals/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Counter from './components/Counter.vue';
+</script>
+
+<template>
+	<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	<p>hola mundo</p>
+	<Counter/>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+</style>
---
+++ vue-fherrera/03-fundamentals/src/components/Counter.vue
@@ -0,0 +1,63 @@
+<template>
+	<h2>Counter!!</h2>
+	<!-- <p>{{ counter }} <sup>2</sup> = {{ counter * counter }} </p>
+	<p>{{ counter }} <sup>2</sup> = {{ getSquareValue() }} </p> -->
+
+	<p>{{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
+
+	<div>
+		<button @click="increase">+1</button>
+		<button @click="decrease">-1</button>
+	</div>
+</template>
+
+<script>
+export default {
+	// name: 'Hola',
+	data() {
+		return {
+			counter: 5
+		}
+	},
+	methods: {
+		getSquareValue(){
+			console.log('getSquareValue')
+			return this.counter * this.counter
+		},
+		increase(){
+			// this.counter += 1
+			this.counter++
+		},
+		decrease() {
+			// this.counter -= 1
+			this.counter--
+		}
+	}, 
+	computed:{
+		squareCounter(){
+			console.log('computed')
+			return this.counter * this.counter
+		}
+	}
+}
+</script>
+
+<style>
+
+button {
+	background-color: #64B687;
+	border-radius: 5px;
+	border: 1px solid white;
+	color: white;
+	cursor: pointer;
+	margin: 0 5px;
+	padding: 5px 15px;
+	transition: 0.3s ease-in-out;
+}
+
+button:hover {
+	background-color: #478060;
+	transition: 0.3s ease-in-out;
+}
+
+</style>
