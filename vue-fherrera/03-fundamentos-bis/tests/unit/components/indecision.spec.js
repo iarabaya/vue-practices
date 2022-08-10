f53a23aaec2df0e66540f162c905d6f49562c560
iara baya <iarabayav@gmail.com>
2022-08-10 17:53:58 -0300

    finalizacion de tests unitarios

--- vue-fherrera/03-fundamentos-bis/tests/unit/components/indecision.spec.js
+++ vue-fherrera/03-fundamentos-bis/tests/unit/components/indecision.spec.js
@@ -7,7 +7,13 @@ describe( 'Indecision Component', () => {
 	let wrapper
 	let clgSpy
 
-	global.fetch = jest.fn()
+	global.fetch = jest.fn( () => Promise.resolve({
+		json: () => Promise.resolve({
+			answer: 'yes',
+			forced: false,
+			image: 'https://yesno.wtf/assets/yes/2.gif'
+		})
+	}) )
 
 	beforeEach(() =>{
 		wrapper = shallowMount( Indecision )
@@ -40,12 +46,25 @@ describe( 'Indecision Component', () => {
 		expect( getAnswerSpy ).toHaveBeenCalled()
 	})
 
-	test('pruebas en getAnswer',() => {
-			
+	test('pruebas en getAnswer', async () => {
+		await wrapper.vm.getAnswer()
+		console.log(wrapper.vm.img)
+		console.log(wrapper.vm.answer)
+
+		const img = wrapper.find('img')
+		expect( img.exists() ).toBeTruthy()
+		expect( wrapper.vm.img ).toBe('https://yesno.wtf/assets/yes/2.gif')
+		expect( wrapper.vm.answer ).toBe('Si')
 	})
 
-	test('pruebas en getAnswer - Fallo en el API',() => {
-			
+	test('pruebas en getAnswer - Fallo en el API', async () => {
+		fetch.mockImplementationOnce( () => Promise.reject('API is down'))
+		
+		await wrapper.vm.getAnswer()
+		const img = wrapper.find('img')
+
+		expect( img.exists() ).toBeFalsy() 
+		expect( wrapper.vm.answer ).toBe('No se pudo cargar del API')
 	})
 
 
