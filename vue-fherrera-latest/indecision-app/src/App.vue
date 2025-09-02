18264c7779bf7c0200c0600b66773afc613cf11b
Iara Baya <iarabayav@gmail.com>
2025-09-02 00:14:20 -0300

    feat: inicio seccion 5

--- vue-fherrera-latest/indecision-app/src/App.vue
+++ vue-fherrera-latest/indecision-app/src/App.vue
@@ -1,21 +1,12 @@
 <template>
-  <h1 class="text-3xl font-bold">Mi primera App de Vue</h1>
-  <hr>
-
-  <MyCounter :value="5" text="Hola mundo"/>
-  <MyCounterScript :value="6"/>
-  <MyCounterScript2 :value="10"/>
+  <IndecisionView/>
 </template>
 
 <script setup lang="ts">
-import MyCounter from "./components/MyCounter.vue";
-import MyCounterScript from "./components/MyCounterScript.vue";
-import MyCounterScript2 from "./components/my-counter-script/MyCounterScript2.vue";
+import IndecisionView from '@/views/IndecisionView.vue';
+
 
 </script>
 
 <style scoped>
-h1 {
-  color: green;
-}
 </style>
