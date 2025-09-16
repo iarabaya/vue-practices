2a0ba343b90af918b6b89d400ffd4d1a218af5e5
Iara Evelyn Baya Vargas <iarabayav@gmail.com>
2025-09-16 02:51:01 -0300

    feat: avanzo curso vue fherrera

--- vue-fherrera-latest/indecision-app/src/components/chat/ChatMessages.vue
+++ vue-fherrera-latest/indecision-app/src/components/chat/ChatMessages.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="flex-1 overflow-y-auto p-4">
     <div class="flex flex-col space-y-2">
-      <!-- Messages go here -->
-      <ChatBubble :its-mine="true" message="Hola mundo"/>
-      <ChatBubble :its-mine="false" message="Hola mundo"/>
-      <ChatBubble :its-mine="false" message="Hola mundo"/>
-      <ChatBubble :its-mine="true" message="Hola mundo"/>
-      <ChatBubble :its-mine="false" message="Hola mundo"/>
-      <ChatBubble :its-mine="true" message="Hola mundo"/>
-      <ChatBubble :its-mine="false" message="Hola mundo"/>
+      <!-- Messages go here , con shortcut de bind -->
+      <ChatBubble
+        v-for="message in messages"
+        :key="message.id"
+        v-bind="message"
+        />
+        <!-- :its-mine="message.itsMine"
+        :message="message.message"
+        :image="message.image" -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
 import ChatBubble from './ChatBubble.vue';
+
+interface Props {
+  messages: ChatMessage[];
+}
+
+defineProps<Props>();
 </script>
--- vue-fherrera-latest/indecision-app/src/views/IndecisionView.vue
+++ vue-fherrera-latest/indecision-app/src/views/IndecisionView.vue
@@ -1,22 +1,43 @@
-
 <template>
   <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
     <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
-      <span>Mi esposa</span>
+      <span>Mi señora</span>
     </div>
 
-<!-- Chat messages -->
- <ChatMessages/>
+    <!-- Chat messages -->
+    <ChatMessages :messages="messages" />
 
- <!-- MessageBox -->
-  <MessageBox/>
-
-   
+    <!-- MessageBox -->
+    <MessageBox />
   </div>
 </template>
 
 <script setup lang="ts">
 import ChatMessages from '@/components/chat/ChatMessages.vue';
 import MessageBox from '@/components/chat/MessageBox.vue';
+import type { ChatMessage } from '@/interfaces/chat-message.interface';
+import { ref } from 'vue';
 
+const messages = ref<ChatMessage[]>([
+  {
+    id: new Date().getTime(),
+    message: 'Hola mundo!!',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'Hola mundo',
+    itsMine: false,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'Quieres ir a tomar café?',
+    itsMine: true,
+  },
+    {
+    id: new Date().getTime() + 1,
+    message: 'no',
+    itsMine: false,
+  }
+]);
 </script>
