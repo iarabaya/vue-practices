6b4e2a7fe03712ce00752345121c2888044ba5cd
Iara Baya <iarabayav@gmail.com>
2025-09-21 23:53:04 -0300

    feat: inicio seccion 7 pokemon game

---
+++ vue-fherrera-latest/04-pokemon-game/src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="" class="brightness-0 h-[200px]">
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+img {
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+</style>
