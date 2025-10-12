77ef8216ab7755203f095d45060bf2a3d0aa5b60
Iara Baya <iarabayav@gmail.com>
2025-10-12 01:21:03 -0300

    feat: mitad seccion 9, rutas base anidadas

---
+++ vue-fherrera-latest/05-vue-spa/src/modules/landing/layouts/LandingLayout.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <div>
+        <RouterLink class="flex items-center gap-2 font-semibold" to="/">
+          <img src="@/assets/logo.svg" alt="Vue logo" class="logo" width="30" height="40" />
+        </RouterLink>
+      </div>
+      <nav class="ml-auto space-x-4 flex items-center h-10 sm:space-x-6">
+        <RouterLink
+          class="bg-blue-500 p-2 rounded-xl text-white font-bold hover:bg-blue-700"
+          :to="{ name: 'home' }"
+          >Home</RouterLink
+        >
+        <RouterLink
+          class="bg-blue-500 p-2 rounded-xl text-white font-bold hover:bg-blue-700"
+          to="/contact"
+          >Contact</RouterLink
+        >
+        <RouterLink
+          class="bg-blue-500 p-2 rounded-xl text-white font-bold hover:bg-blue-700"
+          to="/features"
+          >Features</RouterLink
+        >
+        <RouterLink
+          class="bg-blue-500 p-2 rounded-xl text-white font-bold hover:bg-blue-700"
+          to="/pricing"
+          >Pricing</RouterLink
+        >
+      </nav>
+    </header>
+    <!-- Fin Header -->
+
+    <!-- Main -->
+    <main class="flex-1 flex items-center justify-center">
+      <RouterView />
+    </main>
+    <!-- Fin Main -->
+
+    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <p class="flex-1 text-sm text-gray-500 text-center">
+        {{ new Date().getFullYear() }} Acme Corporation. Derechos reservados.
+      </p>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue-fherrera-latest/05-vue-spa/src/router/index.ts
@@ -0,0 +1,43 @@
+import HomePage from '@/modules/landing/pages/HomePage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+      ],
+    },
+
+    // Auth
+    {
+      path: '/auth',
+      component: () => import('@/modules/auth/pages/LoginPage.vue'),
+    },
+  ],
+});
+
+export default router;
