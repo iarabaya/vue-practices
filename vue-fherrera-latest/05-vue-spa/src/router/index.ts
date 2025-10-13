9b576f782f6319f7f6bcfeb5703ed3ee34a30e88
Iara Baya <iarabayav@gmail.com>
2025-10-12 23:47:00 -0300

    feat: add login and register page

--- vue-fherrera-latest/05-vue-spa/src/router/index.ts
+++ vue-fherrera-latest/05-vue-spa/src/router/index.ts
@@ -35,7 +35,20 @@ const router = createRouter({
     // Auth
     {
       path: '/auth',
-      component: () => import('@/modules/auth/pages/LoginPage.vue'),
+      redirect: { name: 'login' },
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
     },
   ],
 });
