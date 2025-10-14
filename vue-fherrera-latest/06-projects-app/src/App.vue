d23dd1abf57cb2aa5567fa327e348dde6c7dc335
Iara Baya <iarabayav@gmail.com>
2025-10-14 02:04:58 -0300

    feat: inicio seccion 11 setup

---
+++ vue-fherrera-latest/06-projects-app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <RouterView />
+</template>
+
+<style scoped></style>
