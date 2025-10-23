97bbe9b5b1d66a269bcddb0118028f5f18f4e436
Iara Baya <iarabayav@gmail.com>
2025-10-23 06:25:25 -0300

    feat: setup proyecto seccion 14 admin-shop

---
+++ vue-fherrera-latest/07-admin-shop/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-3xl font-bold underline">You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<style scoped></style>
